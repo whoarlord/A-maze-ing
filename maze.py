03f17ffabc2563dce06a9046101b3b79f485ca71
iker <arrieniker4@gmail.com>
2026-04-16 16:16:35 +0200

    libreria de minilibx, makefile para descargar dependencias y aalgoritmo de prim para generacion de mapa

--- maze.py
+++ maze.py
@@ -6,6 +6,35 @@ class Cell:
         self.E = 0
         self.W = 0
 
+    def cover_all(self):
+        self.visited = True
+        self.N = 1
+        self.S = 1
+        self.E = 1
+        self.W = 1
+
+    def uncover_dir(self, visiting_cell: "Cell", dir: int) -> None:
+        visiting_cell.cover_all()
+        if dir == 0:
+            self.N = 0
+            visiting_cell.S = 0
+        elif dir == 1:
+            self.E = 0
+            visiting_cell.W = 0
+        elif dir == 2:
+            self.S = 0
+            visiting_cell.N = 0
+        elif dir == 3:
+            self.W = 0
+            visiting_cell.E = 0
+        else:
+            raise ValueError("Unknow direction")
+
+    def __str__(self):
+        result: int = self.N * 1 + self.E * 2 + self.S * 4 + self.W * 8
+        hexadeimal: str = "0123456789ABCDEF"
+        return f"{hexadeimal[result]}"
+
 
 class Maze:
 
@@ -27,3 +56,43 @@ class Maze:
                 row.append(Cell())
             result.append(row)
         return result
+
+    def get_cell(self, x: int, y: int) -> Cell:
+        return self.maze_map[y][x]
+
+    def not_visited_neighbours(self, x: int, y: int) -> list[int]:
+        """
+        dir = 0: move N
+        dir = 1: move E
+        dir = 2: move S
+        dir = 3: move W
+        """
+        result: list[int] = []
+        if (y - 1 >= 0 and not self.get_cell(x, y - 1).visited):
+            result.append(0)
+        if (x + 1 < self.width and not self.get_cell(x + 1, y).visited):
+            result.append(1)
+        if (y + 1 < self.height and not self.get_cell(x, y + 1).visited):
+            result.append(2)
+        if (x - 1 >= 0 and not self.get_cell(x - 1, y).visited):
+            result.append(3)
+        return result
+
+    def get_coord_by_dir(self, x: int, y: int, dir: int) -> tuple[int, int]:
+        if dir == 0:
+            return x, y - 1
+        elif dir == 1:
+            return x + 1, y
+        elif dir == 2:
+            return x, y + 1
+        elif dir == 3:
+            return x - 1, y
+        else:
+            raise ValueError("Unknow direction")
+
+    def print_map(self):
+        for i in range(self.height):
+            for j in range(self.width):
+                print(self.maze_map[i][j], end="")
+            print("")
+        print()
